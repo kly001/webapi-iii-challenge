e90a7020b5cb70d1968386ab2545a58aa818d409
kly001 <karenly1961@gmail.com>
2019-11-06 16:03:46 -0500

    Added morgan

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require("morgan");
 
 const server = express();
 
 
 server.use(express.json())
+server.use(morgan("dev"))
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
