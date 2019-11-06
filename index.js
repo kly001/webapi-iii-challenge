46758bc8a33d7e033e3c543a019853016aad6813
kly001 <karenly1961@gmail.com>
2019-11-06 14:51:19 -0500

    Initial Commit; index.js & server.js working

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require("./server.js")
+
+const PORT = process.env.PORT || 5050;
+
+server.listen(PORT, () => {
+    console.log(`**Server listening on port ${PORT}***`);
+});
