97827fd0be8d4bc6f57019a1dd02e095376a3fab
kly001 <karenly1961@gmail.com>
2019-11-06 20:52:59 -0500

    POST "/"

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,11 +1,23 @@
 const express = "express";
 const router = require("express").Router();
 const Users = require("./userDb.js")
+const Posts = require("../posts/postDb.js")
 
 
-router.post("/", (req, res) => {
-
-});
+router.post("/",(req,res) => {
+    const userInfo = req.body
+    if(!userInfo.name) {
+        res.status(400).json({errorMessage:"Please provide a name for the user."})
+    } else {
+        Users.insert(userInfo) 
+        .then(user => {
+            res.status(201).json(user)
+        })
+    .catch(err => {
+        res.status(500).json({error: "There was an error while saving the post to the database."})
+    })
+    }
+})
 
 //----------------------------------------------------------------
 
@@ -47,7 +59,7 @@ router.get("/:id/posts", (req, res) => {
     const {id} = req.params
     Users.getUserPosts(id)
     .then(posts => {
-        if(posts) {
+        if(posts.length>0) {
             res.status(200).json(posts)
         } else { 
             res.status(404).json({message:"User posts do not exist"})
